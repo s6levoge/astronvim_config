f3b5b96dc53d2482758c40b7722a2877d88930ac
Leon VogeL <s6levoge@uni-bonn.de>
2023-12-28 22:31:29 +0100

    add onenord as colorscheme

--- init.lua
+++ init.lua
@@ -18,7 +18,7 @@ return {
   },
 
   -- Set colorscheme to use
-  colorscheme = "astrodark",
+  colorscheme = "onenord",
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
---
+++ plugins/onenord.lua
@@ -0,0 +1,5 @@
+return {
+  -- You can also add new plugins here as well:
+  -- Add plugins, the lazy syntax
+  "rmehri01/onenord.nvim",
+}
